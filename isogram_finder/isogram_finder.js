e44d280289fdc1d7d9f000216aa86b0ab9327576
Paul Wishart <misterwishart@gmail.com>
2018-11-21 17:43:29 +0000

    Isogram completed

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,22 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.toLowerCase().split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
+  let temp_array = this.word.slice(0);
+  temp_array.shift();
 
+
+  const repeatedLetters = this.word.every((letter) => {
+    if (temp_array.includes(letter)){
+      return false;
+    } else {
+      temp_array.shift();
+      return true;
+    }
+  });
+
+  return repeatedLetters;
 }
 
 module.exports = IsogramFinder;
