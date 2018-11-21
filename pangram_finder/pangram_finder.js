bf45a1d248340aaaa5568b106e3ccd6555c0185a
Paul Wishart <misterwishart@gmail.com>
2018-11-21 17:09:26 +0000

    Completed Pangram finder

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,15 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase();
 }
 
 PangramFinder.prototype.isPangram = function () {
 
+  const word_array = this.phrase.split('');
+
+  return this.alphabet.every(function (letter) {
+    return word_array.includes(letter);
+  })
 }
 
 module.exports = PangramFinder;
